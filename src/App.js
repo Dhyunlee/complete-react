736b2bbc8ad7ea3b8993c1485f670cd9194d6800
Donghyun Lee <skyblue5030@naver.com>
2022-05-12 00:00:50 +0900

    jsx 변경해서 화면에 출력하기

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import './App.css';
 
 function App() {
-  return <div className="App">리액트 시작하기</div>;
+  return (
+    <div className="App">
+      <h1>리액트 시작하기</h1>
+      <p>리액트를 공부해봅시다.</p>
+    </div>
+  );
 }
 
 export default App;
